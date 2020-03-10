a849e97d9b344799a327481fd9f6e5563d4b96db
Runtime.Ali <runtimeone@protonmail.com>
2020-03-10 19:28:30 -0400

    fixed member counter and embeded ping command

--- Lumberjack.py
+++ Lumberjack.py
@@ -10,7 +10,9 @@ async def on_ready():
 
 @client.command()
 async def ping(ctx):
-    await ctx.send(f'Pong! {round(client.latency * 1000)}ms')
+    embed=discord.Embed(title='**Ping**', description=f'Pong! {round(client.latency * 1000)}ms')
+    embed.set_author(name=f"{client.user.name}", icon_url=client.user.avatar_url)
+    await ctx.send(embed=embed)
 
 format_date = '%b %d, %Y'
 format_time = '%I:%M %p'
@@ -21,15 +23,16 @@ format_datetime = '%b %d, %Y  %I:%M %p'
 @client.event
 async def on_member_join(member):
     logs = client.get_channel(686365631217533153)
-    current_time = datetime.now()
-    account_age = current_time - member.created_at
-    if (account_age.days + 1) < 7:
-        embed=discord.Embed(title=f'**User Joined**', description=f"Name: {member.mention} ({member.id})\nCreated on: {member.created_at.strftime(format_date)}\nAccount age: {account_age.days + 1} days old\n\n**New Account**", color=0xffc704)
+    account_age = datetime.utcnow() - member.created_at
+    if (account_age.days) == 0:
+        embed=discord.Embed(title=f'**User Joined**', description=f"Name: {member.mention} ({member.id})\nCreated on: {member.created_at.strftime(format_date)}\nAccount age: {account_age.days} days old\n\n**New Account**\nCreated {account_age.seconds//3600} hours {(account_age.seconds%3600)//60} minutes {((account_age.seconds%3600)%60)} seconds" , color=0xffc704)
+    elif 0 < (account_age.days + 1) < 7:
+        embed=discord.Embed(title=f'**User Joined**', description=f"Name: {member.mention} ({member.id})\nCreated on: {member.created_at.strftime(format_date)}\nAccount age: {account_age.days} days old\n\n**New Account**\nCreated {account_age.days +1} days {account_age.seconds//3600} hours {(account_age.seconds%3600)//60} minutes {((account_age.seconds%3600)%60)} seconds" , color=0xffc704)
     else:
-        embed=discord.Embed(title=f'**User Joined**', description=f"Name: {member.mention} ({member.id})\nCreated on: {member.created_at.strftime(format_date)}\nAccount age: {account_age.days + 1} days old", color=0x008000)
+        embed=discord.Embed(title=f'**User Joined**', description=f"Name: {member.mention} ({member.id})\nCreated on: {member.created_at.strftime(format_date)}\nAccount age: {account_age.days} days old", color=0x008000)
     embed.set_author(name=f'{member.name}#{member.discriminator}')
     embed.set_thumbnail(url=member.avatar_url)
-    embed.set_footer(text=f'')
+    embed.set_footer(text=f'Total Members: {member.guild.member_count}')
     embed.timestamp = datetime.utcnow()
     await logs.send(embed = embed)
 
@@ -38,15 +41,12 @@ async def on_member_join(member):
 @client.event
 async def on_member_remove(member):
     logs = client.get_channel(686365631217533153)
-    current_time = datetime.now()
-    account_created_date = member.created_at
-    account_join_date = member.joined_at
-    account_age = current_time - account_created_date
-    time_on_server = current_time - account_join_date
-    embed=discord.Embed(title=f'**User Left**', description=f"Name: {member.mention} ({member.id})\nCreated on: {account_created_date.strftime(format_date)}\nAccount age: {account_age.days + 1} days old\nJoined on: {account_join_date.strftime(format_date)} ({time_on_server.days + 1} days ago)", color=0xd90000)
+    account_age = datetime.utcnow() - member.created_at
+    time_on_server = datetime.utcnow() - member.joined_at
+    embed=discord.Embed(title=f'**User Left**', description=f"Name: {member.mention} ({member.id})\nCreated on: {member.created_at.strftime(format_date)}\nAccount age: {account_age.days} days old\nJoined on: {member.joined_at.strftime(format_date)} ({time_on_server.days} days ago)", color=0xd90000)
     embed.set_author(name=f'{member.name}#{member.discriminator}')
     embed.set_thumbnail(url=member.avatar_url)
-    embed.set_footer(text=f'')
+    embed.set_footer(text=f'Total Members: {member.guild.member_count}')
     embed.timestamp = datetime.utcnow()
     roles = [f'<@&{role.id}>' for role in member.roles[1:]]
     roles_str = " ".join(roles)
